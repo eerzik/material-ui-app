8d695fa3795a4efb8aaf8054cfbfa28537d48039
Engin ERZİK <erzik.engin@gmail.com>
2023-06-19 16:45:41 +0300

    Drawer Oluşturma

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,22 @@
+import { Drawer, Typography } from "@mui/material";
 
+const Layout = ({ children }) => {
 
-const Layout = ({children}) => {
+    const drawWidth=240
 
     return (
         <div>
-           <div>app bar</div>
-           <div>side drawer</div>
-           <div> {children} </div>
+            <div>app bar</div>
+            <Drawer  style={{with:{drawWidth}}} variant="permanent" anchor="left">
+                <div style={{with:{drawWidth}}}>
+                    <Typography variant="h5" style={{ fontSize:'25px'}} >
+                        One Piece Notlar
+                    </Typography>
+                </div>
+            </Drawer>
+
+            <div style={{ backgroundColor: '#f9f9f9', width: '100%' }} > {children} </div>
+
         </div>
     );
 };
