b1b5755bc40eac9f58c8bc3a957345e923b5f069
Engin ERZİK <erzik.engin@gmail.com>
2023-06-19 14:43:36 +0300

    Form Oluşturma

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import { Container, TextField } from '@mui/material';
+import { useState } from 'react';
+import { Container, Button, TextField } from '@mui/material';
+
+
 
 export default function Create() {
+
+    const [title, setTitle] = useState('');
+    const [details, setDetails] = useState('');
+    const [titleError, settTitleError] = useState('');
+    const [detailsError, setDetailsError] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       settTitleError(false)
+       setDetailsError(false)
+       if(title==='')
+       {
+        settTitleError(true)
+       }
+       if(details==='')
+       {
+        setDetailsError(true)
+       }
+
+       if(title&&details)
+       {
+        console.log(title,details)
+       }
+    }
     return (
         <Container>
-            <form>
-            <TextField id="outlined-basic" label="Not Giriniz" color='primary' fullWidth required type='number' multiline maxRows={4} minRows={2}  variant="standard" />
+            <form noValidate error="false" autoComplete="off" onSubmit={ handleSubmit} >
+                <TextField  onChange={(e)=>setTitle(e.target.value)}  
+                 error={titleError} variant='standard' label='Not Başlık' fullWidth margin='normal' />
+                <br/>
+                <TextField onChange={(e)=>setDetails(e.target.value)}   
+                error={detailsError} variant='standard' label='Not Detay' fullWidth margin='normal' />
+                <br/>
+                <Button type='submit' variant='contained' color='primary' margin='normal' >Oluştur</Button>
             </form>
         </Container>
     )
