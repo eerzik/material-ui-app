b83eb8ceec3301f1b01b24f8e652c4f114ee0071
Engin ERZİK <erzik.engin@gmail.com>
2023-06-19 14:14:13 +0300

    Custom Theme Oluşturma

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Create from './pages/Create';
 import Notes from './pages/Notes';
-
+import { createTheme, ThemeProvider } from '@mui/material'
+import {cyan} from '@mui/material/colors'
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f39c12'
+    },
+    secondary:cyan,
+   
+  },
+  typography:{
+      fontFamily:'Big Shoulders Display',
+      fontWeightLight:400,
+      fontWeightRegular:500,
+      fontWeightMedium:600,
+      fontWeightBold:700
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Notes/>} ></Route>
-      <Route path='create' element={<Create/>} ></Route>
-     </Routes>
-     </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme} >
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Notes />} ></Route>
+          <Route path='create' element={<Create />} ></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {  Container,Button } from '@mui/material';
+import {  Container,Button, Typography } from '@mui/material';
 import AnnouncementOutlinedIcon from '@mui/icons-material/AnnouncementOutlined';
 const Create = () => {
     return (
         //Container sağdan soldan boşuklar oluştu
         <Container>
-            <Button variant='outlined' startIcon={<AnnouncementOutlinedIcon/>} >Yeni Kayıt</Button>
-         <AnnouncementOutlinedIcon  fontSize='large' ></AnnouncementOutlinedIcon>
+            <Button color="primary" variant='outlined' startIcon={<AnnouncementOutlinedIcon/>} >Yeni Kayıt</Button>
+         <AnnouncementOutlinedIcon color='secondary'  fontSize='large' ></AnnouncementOutlinedIcon>
+         <Typography color='secondary' > Yeni Kayıt </Typography>
         </Container>
     );
 };
